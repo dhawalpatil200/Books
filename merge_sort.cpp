4c69d4152ece6c2c955964ebac87cffdbe78574a
dhawalpatil200 <dhawalpatil200@gmail.com>
2021-03-18 16:41:05 +0530

    first commit

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,34 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void merge(int arr[],int l, int m, int r){
-
-	int i=l;
-	int j=m+1;
-	int k=l;
-
-	int temp[r-l+1];
-
-	while(i<=m && j<=r){
-		if(arr[i]<=arr[j])
-			temp[k++]=arr[i++];
-			
-		else{
-			temp[k++]=arr[j++];
-		}
-	}
-
-	while(i<=m){
-		temp[k++]=arr[i++];
-	}
-	while(j<=r){
-		temp[k++]=arr[j++];
-	}
-
-	for(int s=l;s<=r;s++)
-		arr[s]=temp[s];
-
-}
 
 void mergesort(int arr[],int l,int r){
 
